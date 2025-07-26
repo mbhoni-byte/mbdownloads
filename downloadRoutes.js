e032a90125fe6c54beb92382f6df76e6e299131f
mbhoni-byte <mbhonibernice@gmail.com>
2025-07-26 12:24:10 +0200

    Delete downloadRoutes.js

--- downloadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, checkPremium } = require('../middleware/auth');
-const { processDownload } = require('../controllers/downloadController');
-
-router.post('/', protect, checkPremium, processDownload);
-
-module.exports = router;
